63b32388d97a1a343a5f39c2edf7e8f5d8600b58
vanessa953 <vanessa@espacomontessoriano.com>
2026-05-07 09:53:07 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,5 @@
+import Login from './pages/Login'
+
 export default function App() {
-  return (
-    <div
-      style={{
-        minHeight: '100vh',
-        background: '#0f7f68',
-        color: 'white',
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-        fontSize: '32px',
-        fontWeight: 'bold'
-      }}
-    >
-      Espaço Montessoriano V2
-    </div>
-  )
+  return <Login />
 }
