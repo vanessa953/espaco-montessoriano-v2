e8c633bebee2bb8fa796f9ccb9ba1a526c4f3b80
vanessa953 <vanessa@espacomontessoriano.com>
2026-05-06 23:05:15 -0300

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+export default function App() {
+  return (
+    <div
+      style={{
+        minHeight: '100vh',
+        background: '#0f7f68',
+        color: 'white',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        fontSize: '32px',
+        fontWeight: 'bold'
+      }}
+    >
+      Espaço Montessoriano V2
+    </div>
+  )
+}
