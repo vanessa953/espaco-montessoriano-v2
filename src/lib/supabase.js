5321efb9d2772004e72e767147e73687ff292a80
vanessa953 <vanessa@espacomontessoriano.com>
2026-05-07 12:25:04 -0300

    Update supabase.js

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,9 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
-const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY
+const supabaseUrl = 'https://SEU-PROJETO.supabase.co'
+const supabaseKey = 'SUA-CHAVE-PUBLICA'
 
-export const supabase = createClient(
-  supabaseUrl,
-  supabaseKey
-)
+export const supabase = createClient(supabaseUrl, supabaseKey)
