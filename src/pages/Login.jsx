5fff5ae6bb6025665df82293c33994046086cf5a
vanessa953 <vanessa@espacomontessoriano.com>
2026-05-07 12:30:45 -0300

    Update Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import { supabase } from '../biblioteca/supabase'
+import { supabase } from '../lib/supabasee'
 
 export default function Login() {
   const [email, setEmail] = useState('')
