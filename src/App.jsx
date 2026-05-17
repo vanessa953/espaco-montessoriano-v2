5472d2b7ed0d229b7d33cd47d4f675f9cffd9b4d
vanessa953 <vanessa@espacomontessoriano.com>
2026-05-17 14:50:50 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
 import Login from './pages/Login'
 import Dashboard from './pages/Dashboard'
+import Pacientes from './pages/Pacientes'
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
+
         <Route path="/" element={<Login />} />
+
         <Route path="/dashboard" element={<Dashboard />} />
+
+        <Route path="/pacientes" element={<Pacientes />} />
+
       </Routes>
     </BrowserRouter>
   )
