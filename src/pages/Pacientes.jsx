b44d3a6b7204712511b9ef2881ba1503407e9377
vanessa953 <vanessa@espacomontessoriano.com>
2026-05-17 14:49:00 -0300

    Create Pacientes.jsx

---
+++ src/pages/Pacientes.jsx
@@ -0,0 +1,8 @@
+export default function Pacientes() {
+  return (
+    <div style={{ padding: 30 }}>
+      <h1>Pacientes</h1>
+      <p>Cadastro e gerenciamento de pacientes.</p>
+    </div>
+  )
+}
