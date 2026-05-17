0dd7ec26c108cc31db917dad9ca737e11a0ce0f8
vanessa953 <vanessa@espacomontessoriano.com>
2026-05-17 13:01:30 -0300

    Update supabase.js

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = 'https://wvbszmjqpfgrhjqpuccu.supabase.co'
-const supabaseKey = 'COLE_A_CHAVE_COMPLETA_AQUI'
+const supabaseKey = 'sb_publishable_xxxxxxxxxxxxxxxxx'
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
