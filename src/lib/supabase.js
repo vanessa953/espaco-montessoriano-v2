bd4f8a1cb8de0fcc647f9296b2f2439ff3a40d26
vanessa953 <vanessa@espacomontessoriano.com>
2026-05-17 13:35:56 -0300

    Update supabase.js

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = 'https://wvbszmjqpfgrhjqpuccu.supabase.co'
-const supabaseKey = 'sb_publishable_xxxxxxxxxxxxxxxxx'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Ind2YnN6bWpxcGZncmhqcXB1Y2N1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzgxMTE3MjIsImV4cCI6MjA5MzY4NzcyMn0.YVm7ddsL9pVvU-iMIe8R1VtmjguyGEeF_XqtanyMBek'
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
